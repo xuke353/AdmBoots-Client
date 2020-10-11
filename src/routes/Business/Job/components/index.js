92be14ef3811887d6ec80919fc6a3485c0c8df6c
xuke <xkry353@163.com>
2020-10-11 21:43:00 +0800

    add  @microsoft/signalr

--- src/routes/Business/Job/components/index.js
+++ src/routes/Business/Job/components/index.js
@@ -9,7 +9,16 @@ import createColumns from './columns';
 import { PlusOutlined, ReloadOutlined } from '@ant-design/icons';
 import './index.less';
 import LogPage from './LogPage';
+import {
+  JsonHubProtocol,
+  HubConnectionState,
+  HubConnectionBuilder,
+  LogLevel
+} from '@microsoft/signalr';
+import store from 'cmn-utils/lib/store';
+
 const { Content, Header } = Layout;
+const IsDev = process.env.NODE_ENV !== 'production';
 
 @connect(({ job, loading }) => ({
   job,
@@ -22,6 +31,81 @@ export default class extends BaseComponent {
     logModalVisible: false,
     preview: false,
   };
+
+  componentDidMount() {
+    //连接signalr
+    this.FnsignalR();
+  }
+  FnsignalR = async () =>{     
+    const user = store.getStore('user');
+    if(user){
+      const token = user.accessToken;
+      console.log(token, "----token signalr---")
+      this.setupSignalRConnection("/api/chathub", token)
+    }    
+  }
+
+  startSignalRConnection = async connection => {
+    try {
+        await connection.start();
+        console.assert(connection.state === HubConnectionState.Connected);
+        console.log('SignalR connection established');
+    } catch (err) {
+        console.assert(connection.state === HubConnectionState.Disconnected);
+        console.error('SignalR Connection Error: ', err);
+        setTimeout(() => this.startSignalRConnection(connection), 5000);
+    }
+};
+//SignalRC
+setupSignalRConnection = (connectionHub, getAccessToken) => {
+    const options = {
+        logMessageContent: IsDev,
+        logger: IsDev ? LogLevel.Warning : LogLevel.Error,
+        accessTokenFactory: () => getAccessToken
+    };
+    // create the connection instance
+    // withAutomaticReconnect will automatically try to reconnect
+    // and generate a new socket connection if needed
+    const connection = new HubConnectionBuilder()
+        .withUrl(connectionHub, options)
+        .withAutomaticReconnect()
+        .withHubProtocol(new JsonHubProtocol())
+        .configureLogging(LogLevel.Information)
+        .build();
+ 
+    // Note: to keep the connection open the serverTimeout should be
+    // larger than the KeepAlive value that is set on the server
+    // keepAliveIntervalInMilliseconds default is 15000 and we are using default
+    // serverTimeoutInMilliseconds default is 30000 and we are using 60000 set below
+    connection.serverTimeoutInMilliseconds = 60000;
+ 
+    // re-establish the connection if connection dropped
+    connection.onclose(error => {
+        console.assert(connection.state === HubConnectionState.Disconnected);
+        console.log('Connection closed due to error. Try refreshing this page to restart the connection', error);
+    });
+ 
+    connection.onreconnecting(error => {
+        console.assert(connection.state === HubConnectionState.Reconnecting);
+        console.log('Connection lost due to error. Reconnecting.', error);
+    });
+ 
+    connection.onreconnected(connectionId => {
+        console.assert(connection.state === HubConnectionState.Connected);
+        console.log('Connection reestablished. Connected with connectionId', connectionId);
+    });
+ 
+    this.startSignalRConnection(connection);
+ 
+    connection.on('hotdata', res => {
+        console.log("SignalR get hot res:", JSON.parse(res))
+        let resdata = JSON.parse(res)
+         
+         
+    });
+ 
+    return connection;
+};
 
   handleReload = () => {
     this.props.dispatch({
